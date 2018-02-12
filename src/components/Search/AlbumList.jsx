48b8b8e5045384007ce8a058e187941a7a739835
anotherjsguy <kuldeepkeshwar@gmail.com>
2018-02-12 18:42:19 +0530

    added material ui & tabs

--- src/components/Search/AlbumList.jsx
+++ src/components/Search/AlbumList.jsx
@@ -1,25 +1,43 @@
-import React from "react";
+import React from 'react';
+import {
+  Table,
+  TableBody,
+  TableHeader,
+  TableHeaderColumn,
+  TableRow,
+  TableRowColumn
+} from 'material-ui/Table';
 
-const rowStyles = {
-  display: "flex",
-  border: "#bb9797 solid 2px",
-  alignItems: "center",
-  margin: "10px",
-};
-const cellStyles = {
-  margin: "10px",
-};
-const containerStyles = {
-  margin: "10px"
-};
-export default ({ results }) => (
-  <div style={containerStyles}>
-    {results.map(album => (
-      <div style={rowStyles} key={album.id}>
-        <div style={cellStyles}>{album.id}</div>
-        <div style={cellStyles}>{album.userId}</div>
-        <div style={cellStyles}>{album.title}</div>
-      </div>
-    ))}
-  </div>
-);
+// const rowStyles = {
+//   display: "flex",
+//   border: "#bb9797 solid 2px",
+//   alignItems: "center",
+//   margin: "10px",
+// };
+// const cellStyles = {
+//   margin: "10px",
+// };
+// const containerStyles = {
+//   margin: "10px"
+// };
+export default ({ results }) =>
+  results.length > 0 ? (
+    <Table>
+      <TableHeader displaySelectAll={false} adjustForCheckbox={false}>
+        <TableRow>
+          <TableHeaderColumn>ID</TableHeaderColumn>
+          <TableHeaderColumn>User Id</TableHeaderColumn>
+          <TableHeaderColumn>Title</TableHeaderColumn>
+        </TableRow>
+      </TableHeader>
+      <TableBody displayRowCheckbox={false}>
+        {results.map(album => (
+          <TableRow key={album.id}>
+            <TableRowColumn>{album.id}</TableRowColumn>
+            <TableRowColumn>{album.userId}</TableRowColumn>
+            <TableRowColumn>{album.title}</TableRowColumn>
+          </TableRow>
+        ))}
+      </TableBody>
+    </Table>
+  ) : null;
