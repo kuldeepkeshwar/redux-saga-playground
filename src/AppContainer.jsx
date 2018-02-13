1ccba52cbdf393b7ed6d43c4e9da206d22392b06
anotherjsguy <kuldeepkeshwar@gmail.com>
2018-02-13 18:20:28 +0530

    added redirect

--- src/AppContainer.jsx
+++ src/AppContainer.jsx
@@ -1,31 +1,26 @@
 import React from 'react';
 import { ConnectedRouter } from 'react-router-redux';
-import { Route, Switch } from 'react-router';
-import { Link } from 'react-router-dom';
+import { Route, Redirect } from 'react-router';
 import { history } from './config';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import Search from './containers/Search';
 import Chat from './containers/Chat';
 import Page from './components/Page';
 
-const SearchPage = ()=><Page Component={Search} />;
+const SearchPage = () => <Page Component={Search} />;
 const ChatPage = () => <Page Component={Chat} />;
+const Home = ()=><Redirect to="/search" />
 
 class AppContainer extends React.Component {
   render() {
     return (
       <MuiThemeProvider>
         <ConnectedRouter history={history}>
-          <Switch>
-            <Route
-              path="/search"
-              component={SearchPage}
-            />
-            <Route
-              path="/chat"
-              component={ChatPage}
-            />
-          </Switch>
+          <div>
+            <Route exact path="/" component={Home} />
+            <Route path="/search" component={SearchPage} />
+            <Route path="/chat" component={ChatPage} />
+          </div>
         </ConnectedRouter>
       </MuiThemeProvider>
     );
